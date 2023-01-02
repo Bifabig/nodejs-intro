d9c7e23ec4e98499737db03f9e223b35afb5b2c5
Bifabig <biftubig@gmail.com>
2023-01-02 16:50:08 +0300

    reading from html pages

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ data: "hello world" }));
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("page/home.html", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.write(data);
+      res.end();
+    });
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    // res.end(JSON.stringify({ data: "hello world" }));
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("page/about.html", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.write(data);
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    fs.readFile("page/404.html", "utf8", (err, data) => {
+      if (err) {
+        throw err;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
 server.listen(8000);
