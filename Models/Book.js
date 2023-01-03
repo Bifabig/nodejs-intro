49a99d7b86f9a7d2bdb082f0c49268ebfb73ba58
Bifabig <biftubig@gmail.com>
2023-01-03 16:08:43 +0300

    mongoose validation done

---
+++ Models/Book.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const BookSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Book", BookSchema, "book");
--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,38 +1,48 @@
 const { ObjectId } = require("mongodb");
-const connect = require("../database/db");
+const Book = require("../Models/Book");
+// const connect = require("../database/db");
 
 exports.index = async (req, res) => {
-  const db = await connect;
-  const books = await db.collection("books").find().toArray();
+  // const db = await connect;
+  // const books = await db.collection("books").find().toArray();
+  const books = await Book.find();
   res.json(books);
 };
 
 exports.store = async (req, res) => {
-  const db = await connect();
-  await db.collection("book").insertOne(req.body);
-  res.status(201).json({ data: "Book is stored" });
+  // const db = await connect();
+  // await db.collection("book").insertOne(req.body);
+  try {
+    await Book.create(req.body);
+    res.status(201).json({ data: "Book is stored" });
+  } catch (error) {
+    res.json(error.errors);
+  }
 };
 
 exports.show = async (req, res) => {
   const _id = ObjectId(req.params.id);
-  const db = await connect();
-  const book = await db.collection("book").find({ _id }).toArray();
+  const book = await Book.find({ _id });
+  // const db = await connect();
+  // const book = await db.collection("book").find({ _id }).toArray();
   res.json(book);
   // res.send(`single book ID: ${req.params.id}`);
 };
 
 exports.update = async (req, res) => {
   const _id = ObjectId(req.params.id);
-  const db = await connect();
-  await db.collection("book").updateOne({ _id }, { $set: req.body });
+  await Book.updateOne({ _id }, { $set: req.body });
+  // const db = await connect();
+  // await db.collection("book").updateOne({ _id }, { $set: req.body });
   res.json({ data: "book is updated" });
   // res.send(`single book ID: ${req.params.id} update`);
 };
 
 exports.delete = async (req, res) => {
   const _id = ObjectId(req.params.id);
-  const db = await connect();
-  await db.collection("book").deleteOne({ _id });
+  await Book.deleteOne({ _id });
+  // const db = await connect();
+  // await db.collection("book").deleteOne({ _id });
   res.status(204).json();
   // res.send(`single book ID: ${req.params.id} delete`);
 };
